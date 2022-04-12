9e8901378654c3160413ea652dcfb49791b4052a
Kharrazi707 <90320815+Kharrazi707@users.noreply.github.com>
2022-04-12 22:27:15 +0700

    Add files via upload

---
+++ BNCC Project/Project.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .my-hover:hover {
+            background: white;
+        }
+
+        .kotak {
+            padding-bottom: 25px;
+            padding-top: 200px;
+        }
+    </style>
+</head>
+<body>
+        <table align="center">
+            <tr>
+                <td>
+                    <h2 style="color: white;" class="kotak">Please Login First</h2>
+                    <input name="username" placeholder="Username">
+                    <br>
+                    <br>
+                    <input type="password" name="password" placeholder="Password">
+                    <br>
+                    <br>
+                    <button type="button" onclick="location.href = 'Page2.html';" class="my-hover">Login</button>
+                </td>
+            </tr>
+        </table>
+    <!-- <input type="submit" value="Login"> -->
+    <!-- onclick="alert('Belom jadi bang')" -->
+    
+
+    
+
+   
+    
+
+    
+    <body style="background-color: rgb(56, 56, 56);"></body>
+
+
+
+
+</body>
+</html>
---
+++ BNCC Project/page2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .paddin {
+            padding-right: 200px;
+        }
+
+        .kotak {
+            
+        }
+        
+    </style>
+</head>
+<body>
+    <h2 style="color: white;">Please choose one of this</h2>
+
+
+
+
+    <body style="background-color: rgb(56, 56, 56);"></body>
+    
+</body>
+</html>
